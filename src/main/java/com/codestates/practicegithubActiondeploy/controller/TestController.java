e52fadebbb960544c9d0ccde6018c1ad22549a8d
Choonsik <moodeary@gmail.com>
2022-08-08 16:11:43 +0900

    55

---
+++ src/main/java/com/codestates/practicegithubActiondeploy/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondeploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String con() {
+        return "성공해라";
+    }
+}
